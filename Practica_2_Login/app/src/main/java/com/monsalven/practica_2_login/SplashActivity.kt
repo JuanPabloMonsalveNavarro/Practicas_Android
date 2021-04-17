59ca8a69cf8d6a56684850e2a6e41015db1f8fd8
JuanPabloMonsalveNavarro <juan.monsalven@udea.edu.co>
2021-04-16 19:38:24 -0500

    Splash con Animación terrninado
    Se realiza el Splashscreen con animación.

--- Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/SplashActivity.kt
+++ Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.monsalven.practica_2_login
 
+import android.app.ActivityOptions
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import java.util.*
 import kotlin.concurrent.timerTask
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +16,17 @@ class SplashActivity : AppCompatActivity() {
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash)
 
-        val timer = Timer()
-        timer.schedule(timerTask { goToMainActivity() }, 2000)
-    }
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
 
-    private fun goToMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
     }
 }
