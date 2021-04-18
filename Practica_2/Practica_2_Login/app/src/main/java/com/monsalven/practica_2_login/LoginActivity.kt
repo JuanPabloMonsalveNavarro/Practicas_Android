6c6e68ccfa5a3c902b7210a14080988aa9482e61
JuanPabloMonsalveNavarro <juan.monsalven@udea.edu.co>
2021-04-18 16:22:27 -0500

    Adición de animaciones
    Se  añaden animaciones a las actividades Login y Registro

--- Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/LoginActivity.kt
+++ Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/LoginActivity.kt
@@ -3,6 +3,11 @@ package com.monsalven.practica_2_login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.monsalven.practica_2_login.databinding.ActivityLoginBinding
 import com.monsalven.practica_2_login.databinding.ActivityRegisterBinding
 
@@ -18,5 +23,23 @@ class LoginActivity : AppCompatActivity() {
         loginBinding.bttnRegister.setOnClickListener {
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
         }
+
+        val ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+        val stb = AnimationUtils.loadAnimation(this, R.anim.stb);
+        val btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+        val btt2 = AnimationUtils.loadAnimation(this, R.anim.btt2);
+        val btt3 = AnimationUtils.loadAnimation(this, R.anim.btt3);
+
+        val main_tittle_login = findViewById(R.id.main_tittle_login) as TextView
+        main_tittle_login.startAnimation(ttb)
+
+        val logo_imageView = findViewById(R.id.logo_imageView) as ImageView
+        logo_imageView.startAnimation(stb)
+
+        val login_cardView = findViewById(R.id.login_cardView) as CardView
+        login_cardView.startAnimation(btt)
+
+        val register_linearLayout = findViewById(R.id.register_linearLayout) as LinearLayout
+        register_linearLayout.startAnimation(btt2)
     }
 }
--- Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/RegisterActivity.kt
+++ Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/RegisterActivity.kt
@@ -2,9 +2,15 @@ package com.monsalven.practica_2_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContentProviderCompat.requireContext
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.monsalven.practica_2_login.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -16,7 +22,14 @@ class RegisterActivity : AppCompatActivity() {
         registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(registerBinding.root)
 
+        val ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+        val btt = AnimationUtils.loadAnimation(this, R.anim.btt);
 
+        val main_register_login = findViewById(R.id.main_register_login) as TextView
+        main_register_login.startAnimation(ttb)
+
+        val login_cardView = findViewById(R.id.login_cardView) as CardView
+        login_cardView.startAnimation(btt)
     }
 
 }
