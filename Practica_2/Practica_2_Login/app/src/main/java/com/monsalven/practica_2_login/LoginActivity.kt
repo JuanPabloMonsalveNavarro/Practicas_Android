82535be6c37d419cc225801f54d5dfbcee4106ec
JuanPabloMonsalveNavarro <juan.monsalven@udea.edu.co>
2021-04-18 14:28:27 -0500

    Formulario de registro modificado
    Se modifican algunos componentes del formulario de registro.

--- Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/LoginActivity.kt
+++ Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.monsalven.practica_2_login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.monsalven.practica_2_login.databinding.ActivityLoginBinding
+import com.monsalven.practica_2_login.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginBinding.root)
+
+        loginBinding.bttnRegister.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
     }
 }
--- Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/RegisterActivity.kt
+++ Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.monsalven.practica_2_login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.monsalven.practica_2_login.databinding.ActivityRegisterBinding
 
@@ -15,13 +16,7 @@ class RegisterActivity : AppCompatActivity() {
         registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(registerBinding.root)
 
-        val genre = resources.getStringArray(R.array.genre_list)
-        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, genre)
-        registerBinding.genreEditText.setAdapter(arrayAdapter)
 
-        val vinculation = resources.getStringArray(R.array.vinculation_list)
-        val arrayAdapter2 = ArrayAdapter(this, R.layout.dropdown_item, vinculation)
-        registerBinding.vinculoEditText.setAdapter(arrayAdapter)
     }
 
 }
