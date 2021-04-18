ef6331f96b0962d9b6f2e391895045395cdd8f4b
JuanPabloMonsalveNavarro <juan.monsalven@udea.edu.co>
2021-04-18 11:53:59 -0500

    Diseño Registro Terminado
    Se realiza el diseño de la actividad que contiene el formulario de registro

--- Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/RegisterActivity.kt
+++ Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/RegisterActivity.kt
@@ -2,10 +2,26 @@ package com.monsalven.practica_2_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.monsalven.practica_2_login.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registerBinding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        registerBinding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(registerBinding.root)
+
+        val genre = resources.getStringArray(R.array.genre_list)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, genre)
+        registerBinding.genreEditText.setAdapter(arrayAdapter)
+
+        val vinculation = resources.getStringArray(R.array.vinculation_list)
+        val arrayAdapter2 = ArrayAdapter(this, R.layout.dropdown_item, vinculation)
+        registerBinding.vinculoEditText.setAdapter(arrayAdapter)
     }
+
 }
