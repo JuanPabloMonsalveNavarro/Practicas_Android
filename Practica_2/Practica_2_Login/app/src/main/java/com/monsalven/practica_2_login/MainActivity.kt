dc8c2c1a091f830c2658e0f3a559314cc9a1cc86
JuanPabloMonsalveNavarro <juan.monsalven@udea.edu.co>
2021-04-20 03:04:39 -0500

    Loggeo de un usuario
    Se implementa el loggeo de un usuario y se despliegan sus datos en un menú lateral.

--- Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/MainActivity.kt
+++ Practica_2/Practica_2_Login/app/src/main/java/com/monsalven/practica_2_login/MainActivity.kt
@@ -1,56 +1,70 @@
 package com.monsalven.practica_2_login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.animation.AnimationUtils
+import android.view.Menu
+import android.view.View
 import android.widget.TextView
-import com.monsalven.practica_2_login.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.monsalven.practica_2_login.usuario_registrado
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
+
+    //val header_view = navigationView.findViewById<TextView>(R.id.textView)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        Log.d("metod", "onCreate")
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val ttb = AnimationUtils.loadAnimation(this,R.anim.ttb)
-        val mainTittleMain = findViewById<TextView>(R.id.main_tittle_main)
-        mainTittleMain.startAnimation(ttb)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val header_view = navigationView.getHeaderView(0)
+
+        val navUsername = header_view.findViewById<TextView>(R.id.textViewUserName) as TextView
+        navUsername.text = usuario_registrado.name
+        val navUseremail = header_view.findViewById<TextView>(R.id.textViewUserEmail) as TextView
+        navUseremail.text = usuario_registrado.email
 
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("metod", "onStart")
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("metod", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("metod", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("metod", "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("metod", "onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("metod", "onDestroy")
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
